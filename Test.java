dd6a886c21fcd5ee8dd34591852b1fc79fda35e6
Bernavschi Natalia <natb@list.ru>
2023-04-06 20:23:56 +0300

    Add 1 file with examples if

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        int x = 10;
+        if(x>=10) {
+            System.out.println("Acest numar > 0");
+        }
+
+    }
+}
